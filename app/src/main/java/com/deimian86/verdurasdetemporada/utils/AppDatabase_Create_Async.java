9413d2f3fab4d6962a999a49e63aab85ef99558e
deimian86 <deimian86@gmail.com>
2017-11-09 08:16:40 +0100

    Data inserted

--- app/src/main/java/com/deimian86/verdurasdetemporada/utils/AppDatabase_Create_Async.java
+++ app/src/main/java/com/deimian86/verdurasdetemporada/utils/AppDatabase_Create_Async.java
@@ -2,20 +2,32 @@ package com.deimian86.verdurasdetemporada.utils;
 
 import android.arch.persistence.room.Room;
 import android.content.Context;
+import android.content.res.Resources;
 import android.os.AsyncTask;
 
+import com.deimian86.verdurasdetemporada.R;
 import com.deimian86.verdurasdetemporada.entities.Verdura;
 
 public class AppDatabase_Create_Async extends AsyncTask<Void, Void, Void> {
 
     private AppDatabase db;
+    private Context context;
 
-    public AppDatabase_Create_Async(AppDatabase db) {
+    public AppDatabase_Create_Async(AppDatabase db, Context context) {
         this.db = db;
+        this.context = context;
     }
 
     @Override
     protected Void doInBackground(Void... voids) {
+
+        Resources res = context.getResources();
+        String[] verdurasNombres = res.getStringArray(R.array.verduras_array);
+
+        for(String s : verdurasNombres) {
+            Verdura v = new Verdura();
+            v.setNombre(s);
+        }
 
         Verdura v1 = new Verdura();
         v1.setNombre("Acelga");
