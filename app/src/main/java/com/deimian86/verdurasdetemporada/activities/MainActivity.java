0f27faab62915721c89ee2edc0f87bd50c024f8a
Damián González <deimian86@gmail.com>
2018-06-26 12:21:00 +0200

    Adding correct month data

--- app/src/main/java/com/deimian86/verdurasdetemporada/activities/MainActivity.java
+++ app/src/main/java/com/deimian86/verdurasdetemporada/activities/MainActivity.java
@@ -124,12 +124,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public CharSequence getPageTitle(int position) {
-            Drawable image = ContextCompat.getDrawable(MainActivity.this, mFragmentImageList.get(position));
-            image.setBounds(0, 0, image.getIntrinsicWidth(), image.getIntrinsicHeight());
-            SpannableString sb = new SpannableString(" " + mFragmentTitleList.get(position));
-            ImageSpan imageSpan = new ImageSpan(image);
-            sb.setSpan(imageSpan, 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-            return sb;
+            return mFragmentTitleList.get(position);
         }
 
 
