715adca61849bbcec3a583fbd713815a7d78c139
Damian <deimian86@gmail.com>
2019-09-02 17:01:33 +0200

    Viewpager deletion improves performance

--- app/src/main/java/com/deimian86/verdurasdetemporada/activities/MainActivity.java
+++ app/src/main/java/com/deimian86/verdurasdetemporada/activities/MainActivity.java
@@ -2,23 +2,17 @@ package com.deimian86.verdurasdetemporada.activities;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-
 import com.deimian86.verdurasdetemporada.BuildConfig;
 import com.deimian86.verdurasdetemporada.fragments.MariscosFragment;
 import com.deimian86.verdurasdetemporada.fragments.PescadosFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import com.deimian86.verdurasdetemporada.R;
 import com.deimian86.verdurasdetemporada.fragments.FrutasFragment;
 import com.deimian86.verdurasdetemporada.fragments.VerdurasFragment;
-import java.util.ArrayList;
-import java.util.List;
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.analytics.Analytics;
 import com.microsoft.appcenter.crashes.Crashes;
@@ -31,83 +25,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        loadFragment(new VerdurasFragment());
         init();
     }
 
     public void init(){
-        final ViewPager mViewPager = findViewById(R.id.pager);
-        setupViewPager(mViewPager);
-
         final BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(
                 new BottomNavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment fragment = null;
                         switch (item.getItemId()) {
                             case R.id.action_verduras:
-                                mViewPager.setCurrentItem(0);
+                                fragment = new VerdurasFragment();
                                 break;
                             case R.id.action_frutas:
-                                mViewPager.setCurrentItem(1);
+                                fragment = new FrutasFragment();
                                 break;
                             case R.id.action_pescados:
-                                mViewPager.setCurrentItem(2);
+                                fragment = new PescadosFragment();
                                 break;
                             case R.id.action_mariscos:
-                                mViewPager.setCurrentItem(3);
+                                fragment = new MariscosFragment();
                                 break;
                         }
-                        return true;
+                        return loadFragment(fragment);
                     }
                 });
     }
 
-    // Inicializacion del adapter de fragments
-    private void setupViewPager(ViewPager viewPager) {
-        MainActivityPagerAdapter adapter = new MainActivityPagerAdapter(getSupportFragmentManager());
-        // Fragment verduras
-        VerdurasFragment fragmentVerduras = new VerdurasFragment();
-        adapter.addFragment(fragmentVerduras, getResources().getString(R.string.tab_verduras));
-        // Fragment Frutas
-        FrutasFragment fragmentFrutas = new FrutasFragment();
-        adapter.addFragment(fragmentFrutas, getResources().getString(R.string.tab_frutas));
-        // Fragment Pescados
-        PescadosFragment fragmentPescados = new PescadosFragment();
-        adapter.addFragment(fragmentPescados, getResources().getString(R.string.tab_pescados));
-        // Fragment Mariscos
-        MariscosFragment fragmentMariscos = new MariscosFragment();
-        adapter.addFragment(fragmentMariscos, getResources().getString(R.string.tab_mariscos));
-        viewPager.setAdapter(adapter);
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 
-    public class MainActivityPagerAdapter extends FragmentPagerAdapter {
-
-        private final List<Fragment> mFragmentList = new ArrayList<>();
-        private final List<String> mFragmentTitleList = new ArrayList<>();
-
-        private MainActivityPagerAdapter(FragmentManager manager) {
-            super(manager);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return mFragmentList.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return mFragmentList.size();
-        }
-
-        private void addFragment(Fragment fragment, String title) {
-            mFragmentList.add(fragment);
-            mFragmentTitleList.add(title);
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return mFragmentTitleList.get(position);
-        }
-
-    }
 }
