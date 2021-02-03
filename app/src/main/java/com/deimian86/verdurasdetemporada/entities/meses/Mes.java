0d2e5d477d67f8642a4c7e7cc66ee2bb52999c64
Damián González <damgonzalez@gobertia.com>
2021-02-03 19:28:42 +0100

    Solving code smells to try automatic sonarcloud analisys

--- app/src/main/java/com/deimian86/verdurasdetemporada/entities/meses/Mes.java
+++ app/src/main/java/com/deimian86/verdurasdetemporada/entities/meses/Mes.java
@@ -12,18 +12,18 @@ public class Mes {
 
     private static int MAX_STR_LENGTH = 3;
 
-    public static int ENERO         = 1;
-    public static int FEBRERO       = 2;
-    public static int MARZO         = 3;
-    public static int ABRIL         = 4;
-    public static int MAYO          = 5;
-    public static int JUNIO         = 6;
-    public static int JULIO         = 7;
-    public static int AGOSTO        = 8;
-    public static int SEPTIEMBRE    = 9;
-    public static int OCTUBRE       = 10;
-    public static int NOVIEMBRE     = 11;
-    public static int DICIEMBRE     = 12;
+    public static final int ENERO         = 1;
+    public static final int FEBRERO       = 2;
+    public static final int MARZO         = 3;
+    public static final int ABRIL         = 4;
+    public static final int MAYO          = 5;
+    public static final int JUNIO         = 6;
+    public static final int JULIO         = 7;
+    public static final int AGOSTO        = 8;
+    public static final int SEPTIEMBRE    = 9;
+    public static final int OCTUBRE       = 10;
+    public static final int NOVIEMBRE     = 11;
+    public static final int DICIEMBRE     = 12;
 
     @PrimaryKey
     private int id;
